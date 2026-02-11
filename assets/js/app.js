333d530524026f64b35c609bfbc7e4d850134753
lordtailo <46997135+lordtailo@users.noreply.github.com>
2026-02-11 22:26:58 +0300

    update assets/js/app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,7 +40,7 @@ const tarihteBugun = [
     { ay: 3, gun: 25, baslik: "Tarihte Bugün", mesaj: "1915: Çanakkale Kara Savaşları başladı. 🛡️" },
     { ay: 4, gun: 29, baslik: "Tarihte Bugün", mesaj: "1953: Türkiye'nin ilk yerli uçağı 'Nu.D.38' Ankara'dan İstanbul'a uçtu. ✈️" },
     { ay: 8, gun: 9, baslik: "Tarihte Bugün", mesaj: "1928: Harf Devrimi'nin ilk adımı atıldı; yeni Türk alfabesi tanıtıldı. ✍️" },
-    { ay: 11, gun: 5, baslik: "Tarihte Bugün", mesaj: "1934: Türk kadınına seçme ve seçilme hakkı tanındı! 🗳️" ,
+    { ay: 11, gun: 5, baslik: "Tarihte Bugün", mesaj: "1934: Türk kadınına seçme ve seçilme hakkı tanındı! 🗳️" },
     { ay: 1, gun: 12, baslik: "Deneme oto bilgilendirme", mesaj: "Denemedir aldırış etmeyin." }
 ];
 
